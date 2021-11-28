bf31f48d5423ee72671a7a967301023ef680316f
Germain Leignel <germain.leignel@gmail.com>
2021-11-28 16:51:00 +0100

    comments

--- app.py
+++ app.py
@@ -5,31 +5,42 @@ import re
 
 app = Flask(__name__)
 
-# read file
+# read data file
 with open('./data/output.json', 'r') as myfile:
     data = myfile.read()
     data = json.loads(data)
 
-
+# only route available
 @app.route("/")
 def index():
+    
+    # get args from url
+    # in the form of /?search=query
     search = request.args.get('search')
 
+    # if no arguments, render with all data
     if not search:
         return render_template('index.html', json=data, length=len(data))
 
-    found = []
+    # create empty list of articles
+    foundArticles = []
+    
+    # iterate over each article in data
     for article in data:
 
+        # search in title, url and description 
         findInTitle = re.search(search, article["title"])
         findInUrl = re.search(search, article["url"])
         findInDescription = re.search(search, article["description"])
-
+        
+        # if at least one match, add the article to `foundArticles`
         if findInTitle or findInDescription or findInUrl:
-            found.append(article)
+            foundArticles.append(article)
 
-    return render_template('index.html', json=found, length=len(found))
+    # render with `foundArticles``
+    return render_template('index.html', json=foundArticles, length=len(foundArticles))
 
 
+# run the app
 if __name__ == '__main__':
     app.run(host='localhost', debug=True)
