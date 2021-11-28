969168099ec538767775bb8266879c17ba523199
Germain Leignel <germain.leignel@gmail.com>
2021-11-28 20:35:32 +0100

    reduced code size

--- app.py
+++ app.py
@@ -22,75 +22,22 @@ def open_rss() -> list:
 
 
 # only route available
-@app.route("/rss")
-def rss():
-    # get args from url
-    # in the form of /?search=query
-    search = request.args.get('search')
-
-    data = open_rss()
-
-    # if no arguments, render with all data
-    if not search:
-        return render_template('index.html', json=data, length=len(data))
-
-    # create empty list of articles
-    found_articles = []
-
-    # iterate over each article in data
-    for article in data:
-
-        # search in title, url and description
-        find_in_title = re.search(search, article["title"])
-        find_in_url = re.search(search, article["url"])
-        find_in_description = re.search(search, article["description"])
-
-        # if at least one match, add the article to `found_articles`
-        if find_in_title or find_in_description or find_in_url:
-            found_articles.append(article)
-
-    # render with `found_articles``
-    return render_template('index.html', json=found_articles, length=len(found_articles))
-
-
-@app.route("/reddit")
-def reddit():
-    # get args from url
-    # in the form of /?search=query
-    search = request.args.get('search')
-
-    data = open_reddit()
-
-    # if no arguments, render with all data
-    if not search:
-        return render_template('index.html', json=data, length=len(data))
-
-    # create empty list of articles
-    found_articles = []
-
-    # iterate over each article in data
-    for article in data:
-
-        # search in title, url and description
-        find_in_title = re.search(search, article["title"])
-        find_in_url = re.search(search, article["url"])
-        find_in_description = re.search(search, article["description"])
-
-        # if at least one match, add the article to `found_articles`
-        if find_in_title or find_in_description or find_in_url:
-            found_articles.append(article)
-
-    # render with `found_articles``
-    return render_template('index.html', json=found_articles, length=len(found_articles))
-
-
 @app.route("/")
+@app.route("/rss")
+@app.route("/reddit")
 def index():
     # get args from url
     # in the form of /?search=query
     search = request.args.get('search')
 
-    data = open_rss() + open_reddit()
+    route = request.path
+
+    if route == "/reddit":
+        data = open_reddit()
+    elif route == "/rss":
+        data = open_rss()
+    else:
+        data = open_reddit() + open_rss()
 
     # if no arguments, render with all data
     if not search:
