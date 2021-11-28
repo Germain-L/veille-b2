91e1159e0237b9c3b29cc3cc7119dc097de5e467
Germain Leignel <germain.leignel@gmail.com>
2021-11-28 18:22:20 +0100

    added reddit

--- app.py
+++ app.py
@@ -1,44 +1,118 @@
-from re import S
 from flask import Flask, render_template, request
 import json
 import re
 
 app = Flask(__name__)
 
-# read data file
-with open('./data/output.json', 'r') as myfile:
-    data = myfile.read()
-    data = json.loads(data)
+
+def open_reddit() -> list:
+    with open('data/output_reddit.json', 'r') as reddit_file:
+        data = reddit_file.read()
+        data = json.loads(data)
+
+        return data
+
+
+def open_rss() -> list:
+    with open('data/output_rss.json', 'r') as rss_file:
+        data = rss_file.read()
+        data = json.loads(data)
+
+        return data
+
 
 # only route available
-@app.route("/")
-def index():
-    
+@app.route("/rss")
+def rss():
     # get args from url
     # in the form of /?search=query
     search = request.args.get('search')
+
+    data = open_rss()
 
     # if no arguments, render with all data
     if not search:
         return render_template('index.html', json=data, length=len(data))
 
     # create empty list of articles
-    foundArticles = []
-    
+    found_articles = []
+
     # iterate over each article in data
     for article in data:
 
-        # search in title, url and description 
-        findInTitle = re.search(search, article["title"])
-        findInUrl = re.search(search, article["url"])
-        findInDescription = re.search(search, article["description"])
-        
-        # if at least one match, add the article to `foundArticles`
-        if findInTitle or findInDescription or findInUrl:
-            foundArticles.append(article)
+        # search in title, url and description
+        find_in_title = re.search(search, article["title"])
+        find_in_url = re.search(search, article["url"])
+        find_in_description = re.search(search, article["description"])
 
-    # render with `foundArticles``
-    return render_template('index.html', json=foundArticles, length=len(foundArticles))
+        # if at least one match, add the article to `found_articles`
+        if find_in_title or find_in_description or find_in_url:
+            found_articles.append(article)
+
+    # render with `found_articles``
+    return render_template('index.html', json=found_articles, length=len(found_articles))
+
+
+@app.route("/reddit")
+def reddit():
+    # get args from url
+    # in the form of /?search=query
+    search = request.args.get('search')
+
+    data = open_reddit()
+
+    # if no arguments, render with all data
+    if not search:
+        return render_template('index.html', json=data, length=len(data))
+
+    # create empty list of articles
+    found_articles = []
+
+    # iterate over each article in data
+    for article in data:
+
+        # search in title, url and description
+        find_in_title = re.search(search, article["title"])
+        find_in_url = re.search(search, article["url"])
+        find_in_description = re.search(search, article["description"])
+
+        # if at least one match, add the article to `found_articles`
+        if find_in_title or find_in_description or find_in_url:
+            found_articles.append(article)
+
+    # render with `found_articles``
+    return render_template('index.html', json=found_articles, length=len(found_articles))
+
+
+@app.route("/")
+def index():
+    # get args from url
+    # in the form of /?search=query
+    search = request.args.get('search')
+
+    data = open_rss() + open_reddit()
+
+    # if no arguments, render with all data
+    if not search:
+        return render_template('index.html', json=data, length=len(data))
+
+    # create empty list of articles
+    found_articles = []
+
+    # iterate over each article in data
+    for article in data:
+
+        # search in title, url and description
+        find_in_title = re.search(search, article["title"])
+        find_in_url = re.search(search, article["url"])
+        find_in_description = re.search(search, article["description"])
+
+        # if at least one match, add the article to `found_articles`
+        if find_in_title or find_in_description or find_in_url:
+            found_articles.append(article)
+
+    # render with `found_articles``
+    return render_template('index.html', json=found_articles, length=len(found_articles))
 
 
 # run the app
--- script.py
+++ script.py
@@ -3,35 +3,52 @@ import io
 import sys
 
 file_name = sys.argv[1]
+feed_type = sys.argv[2]
 
-print(file_name)
-# Opening JSON file
-f = io.open(file_name, mode="r", encoding="utf-8")
 
-# returns JSON object as
-# a dictionary
-data = json.load(f)
+def rss():
+    with io.open(file_name, mode="r", encoding="utf-8") as f:
+        data = json.load(f)
+        messages = []
 
-# Iterating through the json
-# list
+        for message in data["messages"]:
+            for embed in message["embeds"]:
+                new_article = {
+                    'title': embed["title"],
+                    'url': embed["url"],
+                    'description': embed["description"]
+                }
+                messages.append(new_article)
 
-# get all titles
-messages = []
-# for message in data["messages"]:
-#     for embed in message["embeds"]:
-#         print(embed["title"])
+        f.close()
 
-for message in data["messages"]:
-    for embed in message["embeds"]:
-        new_article = {
-            'title': embed["title"],
-            'url': embed["url"],
-            'description': embed["description"]
-        }
-        messages.append(new_article)
+    out = io.open('data\output_rss.json', mode="w", encoding="utf-8")
+    out.write(json.dumps(messages, indent=4))
 
-# Closing file
-f.close()
 
-out = io.open('data\output.json', mode="w", encoding="utf-8")
-out.write(json.dumps(messages, indent=4))
+def reddit():
+    with io.open(file_name, mode="r", encoding="utf-8") as f:
+        data = json.load(f)
+        messages = []
+        for message in data["messages"]:
+            for embed in message["embeds"]:
+                new_article = {
+                    'title': embed["author"]["name"],
+                    'url': embed["author"]["url"],
+                    'description': embed["description"]
+                }
+                messages.append(new_article)
+
+        f.close()
+
+    out = io.open('data\output_reddit.json', mode="w", encoding="utf-8")
+    out.write(json.dumps(messages, indent=4))
+
+
+if feed_type not in ("reddit", "rss"):
+    print("Wrong feed type, only reddit or rss")
+
+elif feed_type == "reddit":
+    reddit()
+elif feed_type == "rss":
+    rss()
